8d6e116d6573f30d7da75fba697fdb7edafcb44f
M Ashraf ul Abedein <aaj.cse@gmail.com>
2018-12-11 17:52:45 +0800

    Create initialState.js

---
+++ src/reducers/initialState.js
@@ -0,0 +1,16 @@
+export default {
+  home: {
+    dateModified: null,
+    title:null,
+    meta:null,
+  },
+  about: {
+    dateModified: null,
+    title:null,
+    meta:null,
+  },
+  common: {
+    user:null,
+    token:null,
+  }
+};
