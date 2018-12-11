892f98922dca32d59cbea37e2268dd4296017611
M Ashraf ul Abedein <aaj.cse@gmail.com>
2018-12-11 17:49:13 +0800

    Create homeReducer.js

---
+++ src/reducers/homeReducer.js
@@ -0,0 +1,16 @@
+import * as types from '../constants/actionTypes';
+import objectAssign from 'object-assign';
+import initialState from './initialState';
+
+export default function homeReducer(state = initialState.home, action) {
+  let newState;
+
+  switch (action.type) {
+    case OPEN_HOME_PAGE:
+      return objectAssign({}, state, {dateModified: action.dateModified});
+    case SOMETHING_HOME_PAGE:
+      return objectAssign({}, state, {dateModified: action.dateModified});
+    default:
+      return state;
+  }
+}
