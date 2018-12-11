490437a06008dba65ac6a829e0eae0aa0ed1d0b7
M Ashraf ul Abedein <aaj.cse@gmail.com>
2018-12-11 17:30:27 +0800

    Create HomePage.jsx

---
+++ src/components/page/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h>
+      <p>React components here</p>
+    </div>
+  );
+};
+
+export default HomePage;
